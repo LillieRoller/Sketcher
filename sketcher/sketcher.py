834633957a0881ef50ab653d7dd86b117fde5085
Aaron Roller <aaron.roller@airtonomy.ai>
2022-11-06 03:53:50 +0000

    dev: python3 dev container and skeleton main

---
+++ sketcher/sketcher.py
@@ -0,0 +1,10 @@
+"""The main program to run with python.
+	ex: python3 sketcher/sketcher.py
+"""
+
+def main():
+	"""Puts together all that is needed to complete a drawing."""
+	print("Hello World!")
+
+if __name__ == "__main__":
+    main()
