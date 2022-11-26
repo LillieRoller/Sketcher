b63e55d8ce56f9a25d94fd58235980cd50afee81
Lillie Roller <lillie.rolle@gmail.com>
2022-11-26 17:00:56 +0000

    created path classes and tests

--- sketcher/svg/path.py
+++ sketcher/svg/path.py
@@ -1,2 +1,33 @@
 """The purpose is to read the svg path and turn into comands and turned into comands that the irobot understands"""
 
+from enum import Enum
+from dataclasses import dataclass
+
+def PathCommand(enum):
+    """defining all comands that come from SVG path element's d attribute"""
+    MOVE = "m"
+    HORIZONTAL = "h"
+    VERTICAl = "v"
+    HOME = "z"
+    LINE = "l"
+
+@dataclass
+class Coordinate:
+    """a location in a 2D space"""
+    x: int
+    y: int
+
+@dataclass
+class Destination:
+    """going from point A to point B"""
+    command: PathCommand
+    coordinate: Coordinate
+
+@dataclass
+class Route:
+    """series of destinations in order"""
+    destinations: list[Destination]
+
+def path_to_route(path_d_attribute:str)-> Route:
+    """reading the path element's d attribute and return coordinates with the enum commands"""
+    return Route
--- tests/svg/path_test.py
+++ tests/svg/path_test.py
@@ -1,2 +1,15 @@
-import sketcher.svg.path as path
+from sketcher.svg.path import path_to_route, Route, Destination, Coordinate, PathCommand
 
+square_path_fixture = "M 10 10 H 90 V 90 H 10 Z"
+
+def test_path_to_route_square_path():
+    # route = path_to_route(square_path_fixture)
+    assert False
+
+def test_path_to_route_move_to_destination():
+    actual = path_to_route(path_d_attribute = "M 0 0")
+    assert len(actual.destinations) == 1
+
+def test_path_to_route_no_destination():
+    actual = path_to_route(path_d_attribute = "")
+    assert len(actual.destinations) == 0
