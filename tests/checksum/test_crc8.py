a448711107a8a14ce159267b5a0b65f44b73d049
Lillie Roller <lillie.rolle@gmail.com>
2023-02-26 11:40:29 +0000

    prove chemsum works in a test

---
+++ tests/checksum/test_crc8.py
@@ -0,0 +1,28 @@
+import crc8
+
+def test_example():
+	hash = crc8.crc8()
+	hash.update(b'123')
+	assert hash.hexdigest() == 'c0'
+	assert hash.digest() == b'\xc0'
+
+def test_drive_forward():
+	hash = crc8.crc8()
+	hash.update(b'\x01\x04\x00\x00\x00\x00\x64\x00\x00\x00\x64\x00\x00\x00\x00\x00\x00\x00\x00')
+	assert hash.hexdigest() == 'd1'
+
+def test_drive_forward_array():
+	bytes = [0x01, 0x04, 0x00, 0x00, 0x00, 0x00, 0x64, 0x00, 0x00, 0x00, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+	hash = crc8.crc8()
+	hash.update(bytearray(bytes))
+	assert hash.hexdigest() == 'd1'
+
+def test_checksum_appended_to_bytearray():
+	data = [0x01, 0x04, 0x00, 0x00, 0x00]
+	expected = [0x01, 0x04, 0x00, 0x00, 0x00, 0x3a]
+	calc = crc8.crc8()
+	calc.update(bytearray(data))
+	actual = data.copy()
+	checksum = int(calc.hexdigest(), 16)
+	actual.append(checksum)
+	assert expected == actual
