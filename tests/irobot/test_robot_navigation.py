6c034e55a437b5b1f10c2a5f1bb253ac29654497
Lillie Roller <lillie.rolle@gmail.com>
2023-04-13 12:48:08 +0200

    corrected the math to work in all 4 quadrants

--- tests/irobot/test_robot_navigation.py
+++ tests/irobot/test_robot_navigation.py
@@ -1,37 +1,67 @@
 from irobot.robot_navigation import Point, LineSegment
 import math
 
-def test_verticle_line():
+''' can never have negative degree angles '''
+
+def test_up():
     start = Point(0,0)
     destination = Point(0,1)
     segment = LineSegment(start, destination)
-    assert segment.angle == 0
-    assert segment.distance == 1
+    assert segment.angle == int(0)
+    assert segment.distance == int(1)
 
-def test_horizontal_line():
-    start = Point(0,0)
-    destination = Point(1, 0)
-    segment = LineSegment(start, destination)
-    assert segment.angle == 90
-    assert segment.distance == 1
-
-def test_diagonal_line():
-    start = Point(0,0)
+def test_up_right():
+    start = Point(0, 0)
     destination = Point(1, 1)
     segment = LineSegment(start, destination)
     assert segment.angle == int(45)
     assert segment.distance == int(math.sqrt(2))
 
-def test_sw_diagonal_line():
+def test_right():
+    start = Point(0, 0)
+    destination = Point(1, 0)
+    segment = LineSegment(start, destination)
+    assert segment.angle == int(90)
+    assert segment.distance == int(1)
+
+def test_right_down():
+    start = Point(0, 0)
+    destination = Point(1, -1)
+    segment = LineSegment(start, destination)
+    assert segment.angle == int(135)
+    assert segment.distance == int(math.sqrt(2))
+
+def test_down():
+    start = Point(0, 0)
+    destination = Point(0, -1)
+    segment = LineSegment(start, destination)
+    assert segment.angle == int(180)
+    assert segment.distance == int(1)
+
+def test_down_left():
     start = Point(0,0)
     destination = Point(-1,-1)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(-135)
+    assert segment.angle == int(225)
     assert segment.distance == int(math.sqrt(2))
 
-def test_nw_diagonal_line():
-    start = Point(0,0)
-    destination = Point(-1,1)
+def test_left():
+    start = Point(0, 0)
+    destination = Point(-1,0)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(-45)
+    assert segment.angle == int(270)
+    assert segment.distance == int(1)
+
+def test_left_up():
+    start = Point(0, 0)
+    destination = Point(-1, 1)
+    segment = LineSegment(start, destination)
+    assert segment.angle == int(315)
     assert segment.distance == int(math.sqrt(2))
+
+def test_return():
+    start = Point(0, 1)
+    destination = Point(0, 0)
+    segment = LineSegment(angle, distance)
+    assert segment.angle == int(180)
+    assert segment.distance == int(1)
