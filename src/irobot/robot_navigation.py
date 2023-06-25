f8c84607264c1693e93036aeeacabb9e265049d7
Lillie Roller <lillie.rolle@gmail.com>
2023-06-25 18:19:28 +0200

    Created math so the robot and navigate to position

--- src/irobot/robot_navigation.py
+++ src/irobot/robot_navigation.py
@@ -9,62 +9,124 @@ class Point:
     x: int
     y: int
 
+def angle_formula(run: float, rise: float, hypot: float) -> float:
+    """calculate the angle to navigate the distance. 
+    Robot wants angle degree from North 
+    """
+    # cos α = [b2 + c2 – a2]/2bc
+    # α = cos-1 (b^2+c^2-a^2)/2bc
+    # α = cos-1 (hypo^2+rise^2-run^2)/2(hypo)(rise)
+    step1 = hypot ** 2 + rise ** 2 - run ** 2
+    step2 = step1 / (2 * hypot * rise)
+    inverse_cos_rad = np.arccos(step2)
+    inverse_cos_deg = math.degrees(inverse_cos_rad)
+    return round(inverse_cos_deg, 1)
+
 class LineSegment:
-    """provides the details to navigate from start to destination"""
+    """provides the details to navigate from start to destination
+    
+    y = rise    d = distance (hyptonenuse)
+            
+            | A/ destination
+            | /
+            |/ start 
+      ---------------  x = run
+            |
+            |
+            |
+    From a start of x,y within the grid, the robot wants angle from North 
+    and distance to travel. 
+    Pythagorean theorum solves for d.
+    A uses law of sines/cosines.
+
+
+    """
+
     def __init__(self, start:Point, destination:Point) -> None:
+        self.angle:int = None
+        self.distance:int = None
+        self.start:Point = start
+        self.destination:Point = destination
+        # solving distance using pythagorean theorum (hypotenuse)
         run = int(destination.x - start.x)
         rise = int(destination.y - start.y)
-        hypo = run**2 + rise**2
-        hypotenuse = math.sqrt(hypo)        
-        step1 = (run**2) - (hypotenuse**2) - (rise**2)
-        step2 = 2*rise*hypotenuse
-        if step2 == 0:
-            step3 = 0
-            inverse_cos_rad = np.arccos(step3)
-            inverse_cos_deg = 90
+        hypo = run**2 + rise **2
+        hypot = math.sqrt(hypo)
+        hypotenuse = round(hypot, 1)
+        self.distance = hypotenuse
+        
+        # direct right special case avoids divide by zero
+        if rise == 0 and run > 0:
+            self.angle = 90
+        # direct left special case avoids divide by zero
+        elif rise == 0 and run < 0:
+            self.angle = 270
+        elif run < 0 and rise > 0:
+            self.angle = 360 - angle_formula(run=run,rise=rise,hypot=hypot)
+        elif run < 0 and rise < 0:
+            self.angle = 360 - angle_formula(run=run,rise=rise,hypot=hypot)
         else:
-            step3 = -1*(step1/step2)
-            inverse_cos_rad = np.arccos(step3)
-            inverse_cos_deg = math.degrees(inverse_cos_rad)
-        angle = round(inverse_cos_deg)
-        if destination.x < 0:
-            adjust_angle = 360 - angle
-            final_angle = adjust_angle
-        else:
-            final_angle = angle
-        self.angle = int(final_angle)
-        self.distance = int(hypotenuse)
+            self.angle = angle_formula(run=run,rise=rise,hypot=hypot)
+            # raise NotImplementedError(f"Not yet handling {start} and {destination}")
+
+
+       
+
+            
+
+        
+
+        # if start.x < destination.x and start.y < destination.y or start.x < destination.x and start.y > destination.y:
+        #     final_angle = inverse_cos_deg
+        # elif start.x > destination.x and start.y > destination.y or start.x > destination.x and start.y < destination.y:
+        #     counter_clockwise_angle = inverse_cos_deg
+        #     final_angle = 360 - counter_clockwise_angle
+        
+        # elif start.x == destination.x and start.y > destination.y:
+        #     final_angle = 180
+        # elif start.x == destination.x and start.y < destination.y:
+        #     final_angle = 0
+        
+        # elif start.x > destination.x and start.y == destination.y:
+        #     final_angle = 270
+        # elif start.x < destination.x and start.y == destination.y:
+        #     final_angle = 90
+
+        # self.angle = int(final_angle)
+        # self.distance = int(hypotenuse)
+        # self.start = start
+        # self.destination = destination
 
     def __str__(self) -> str:
         return f"{self.angle} {self.distance}"
 
 class Tracking:
     """tracks the robot and its current position and direction"""
-    def __init__(self, x: int, y: int, direction: int) -> None:
+    x:int = None
+    y:int = None
+    direction:int = None
+    def __init__(self, x: int, y: int, dir: int) -> None:
         self.x = x
         self.y = y
-        self.direction = direction
+        self.direction = dir
 
-    def update_position(self, distance: int, angle: int) -> None:
-        """updates the robot's position and direction based on a positioning"""
+    def update_position(self, point: Point, dir: int) -> None:
+        """updates the robot's position and direction based on a positioning
+        not taking the direction ther distance  but about the point"""
         # update direction
-        self.direction += angle
-        self.direction %= 360
+        self.x = point.x
+        self.y = point.y
 
         # calculate new position
-        dx = int(distance * math.cos(math.radians(self.direction)))
-        dy = int(distance * math.sin(math.radians(self.direction)))
-        new_x = self.x + dx
-        new_y = self.y + dy
-        return new_x, new_y
+        
+        self.direction = dir + self.direction
 
-def course(path: list[LineSegment]) -> tuple[int, int, int]:
-    """keeps track of the position and direction of the robot as it moves along the path"""
-    track = Tracking(0,0,0)  # initialize robot position and direction
+    def course(self, path: list[LineSegment]) -> None:
+        """keeps track of the position and direction of the robot as it moves along the path"""
 
-    for segment in path:
-        # update robot position and direction based on segment
-        track.update_position(segment.distance, segment.angle)
-
-    # return current position and direction
-    return track.x, track.y, track.direction
+        for segment in path:
+            # update robot position and direction based on segment
+            self.update_position(point = segment.destination, dir = segment.angle)
+        
+class RobotNavigator:
+    """Takes points and moves to diffferent coordinante buy using rotate and distance packets together """
--- tests/irobot/test_robot_navigation.py
+++ tests/irobot/test_robot_navigation.py
@@ -1,78 +1,299 @@
-from irobot.robot_navigation import Point, LineSegment, Tracking, course
+from irobot.robot_navigation import Point, LineSegment, Tracking
 import math
 
 ''' can never have negative degree angles '''
 
-def test_up():
-    start = Point(0,0)
-    destination = Point(0,1)
-    segment = LineSegment(start, destination)
-    assert segment.angle == int(0)
-    assert segment.distance == int(1)
+""" upper right quad """
 
-def test_up_right():
-    start = Point(0, 0)
-    destination = Point(1, 1)
+def test_up_PP():
+    start = Point(1,1)
+    destination = Point(1,5)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(45)
-    assert segment.distance == int(math.sqrt(2))
+    assert segment.angle == 0
+    assert segment.distance == 4
 
-def test_right():
-    start = Point(0, 0)
-    destination = Point(1, 0)
+def test_down_PP():
+    start = Point(1,4)
+    destination = Point(1,2)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(90)
-    assert segment.distance == int(1)
+    assert segment.angle == 180
+    assert segment.distance == 2
 
-def test_right_down():
-    start = Point(0, 0)
-    destination = Point(1, -1)
+def test_left_PP():
+    start = Point(6,5)
+    destination = Point(1,5)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(135)
-    assert segment.distance == int(math.sqrt(2))
+    assert segment.angle == 270
+    assert segment.distance == 5
 
-def test_down():
-    start = Point(0, 0)
-    destination = Point(0, -1)
+def test_right_PP():
+    start = Point(1,4)
+    destination = Point(5,4)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(180)
-    assert segment.distance == int(1)
+    assert segment.angle == 90
+    assert segment.distance == 4
 
-def test_down_left():
-    start = Point(0,0)
+def test_up_right_PP():
+    start = Point(1,4)
+    destination = Point(5,9)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 38.7
+    assert segment.distance == 6.4
+
+def test_down_right_PP():
+    start = Point(1,9)
+    destination = Point(5,4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 141.3
+    assert segment.distance == 6.4
+
+def test_up_left_PP():
+    start = Point(5,4)
+    destination = Point(1,9)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 321.3
+    assert segment.distance == 6.4
+
+def test_down_left_PP():
+    start = Point(5,9)
+    destination = Point(1,4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 218.7
+    assert segment.distance == 6.4
+
+"""lowwer right quad"""
+
+def test_up_PN():
+    start = Point(1,-5)
+    destination = Point(1,-1)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 0
+    assert segment.distance == 4
+
+def test_down_PN():
+    start = Point(1,-2)
+    destination = Point(1,-4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 180
+    assert segment.distance == 2
+
+def test_left_PN():
+    start = Point(6,-5)
+    destination = Point(1,-5)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 270
+    assert segment.distance == 5
+
+def test_right_PN():
+    start = Point(1,-4)
+    destination = Point(5,-4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 90
+    assert segment.distance == 4
+
+def test_up_right_PN():
+    start = Point(1,-9)
+    destination = Point(5,-4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 38.7
+    assert segment.distance == 6.4
+
+
+def test_down_right_PN():
+    start = Point(1,-4)
+    destination = Point(5,-9)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 141.3
+    assert segment.distance == 6.4
+
+def test_up_left_PN():
+    start = Point(5,-9)
+    destination = Point(1,-4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 321.3
+    assert segment.distance == 6.4
+
+def test_down_left_PN():
+    start = Point(5,-4)
+    destination = Point(1,-9)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 218.7
+    assert segment.distance == 6.4
+
+"""Lower left quad"""
+
+def test_up_NN():
+    start = Point(-1,-5)
     destination = Point(-1,-1)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(225)
-    assert segment.distance == int(math.sqrt(2))
+    assert segment.angle == 0
+    assert segment.distance == 4
 
-def test_left():
-    start = Point(0, 0)
-    destination = Point(-1,0)
+def test_down_NN():
+    start = Point(-1,-2)
+    destination = Point(-1,-4)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(270)
-    assert segment.distance == int(1)
+    assert segment.angle == 180
+    assert segment.distance == 2
 
-def test_left_up():
-    start = Point(0, 0)
-    destination = Point(-1, 1)
+def test_left_NN():
+    start = Point(-1,-5)
+    destination = Point(-6,-5)
     segment = LineSegment(start, destination)
-    assert segment.angle == int(315)
-    assert segment.distance == int(math.sqrt(2))
+    assert segment.angle == 270
+    assert segment.distance == 5
 
-def test_line_segment():
-    p1 = Point(0, 0)
-    p2 = Point(1, 1)
-    line_seg = LineSegment(p1, p2)
-    assert line_seg.angle == 45
-    assert line_seg.distance == int(math.sqrt(2))
+def test_right_NN():
+    start = Point(-5,-4)
+    destination = Point(-1,-4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 90
+    assert segment.distance == 4
 
-def test_course():
-    line1 = LineSegment(Point(0, 0), Point(0, 1))
-    line2 = LineSegment(Point(0, 1), Point(1, 1))
-    line3 = LineSegment(Point(1, 1), Point(0, 0))
-    lines = [line1, line2, line3]
-    x, y, direction = course(lines)
-    assert x == 0
-    assert y == 0
-    assert direction == 225
+def test_up_right_NN():
+    start = Point(-5,-9)
+    destination = Point(-1,-4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 38.7
+    assert segment.distance == 6.4
 
+
+def test_down_right_NN():
+    start = Point(-5,-4)
+    destination = Point(-1,-9)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 141.3
+    assert segment.distance == 6.4
+
+def test_up_left_NN():
+    start = Point(-1,-9)
+    destination = Point(-5,-4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 321.3
+    assert segment.distance == 6.4
+
+def test_down_left_NN():
+    start = Point(-1,-4)
+    destination = Point(-5,-9)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 218.7
+    assert segment.distance == 6.4
+
+"""Upper left quad"""
+
+def test_up_NP():
+    start = Point(-1,1)
+    destination = Point(-1,5)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 0
+    assert segment.distance == 4
+
+def test_down_NP():
+    start = Point(-1,4)
+    destination = Point(-1,2)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 180
+    assert segment.distance == 2
+
+def test_left_NP():
+    start = Point(-1,5)
+    destination = Point(-6,5)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 270
+    assert segment.distance == 5
+
+def test_right_NP():
+    start = Point(-5,4)
+    destination = Point(-1,4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 90
+    assert segment.distance == 4
+
+def test_up_right_NP():
+    start = Point(-5,4)
+    destination = Point(-1,9)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 38.7
+    assert segment.distance == 6.4
+
+
+def test_down_right_NP():
+    start = Point(-5,9)
+    destination = Point(-1,4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 141.3
+    assert segment.distance == 6.4
+
+def test_up_left_NP():
+    start = Point(-1,4)
+    destination = Point(-5,9)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 321.3
+    assert segment.distance == 6.4
+
+def test_down_left_NP():
+    start = Point(-1,9)
+    destination = Point(-5,4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 218.7
+    assert segment.distance == 6.4
+
+""" Spanning Quads """
+
+def test_spanning_PP_to_NN():
+    start = Point(1,9)
+    destination = Point(-5,-4)
+    segment = LineSegment(start, destination)
+    assert segment.angle == 204.8
+    assert segment.distance == 14.3
+
+# def test_course():
+#     line1 = LineSegment(Point(0, 0), Point(0, 1))
+#     line2 = LineSegment(Point(0, 1), Point(1, 1))
+#     line3 = LineSegment(Point(1, 1), Point(0, 0))
+#     lines = [line1, line2, line3]
+#     x, y, direction = course(lines)
+#     assert x == 0
+#     assert y == 0
+#     assert direction == 225
+
+def test_tracking_construction():
+    """Making sure what is assigned in cunstruction is what is to be confirmed of the current heding/values"""
+    expected_x = 1
+    expected_y = 5
+    expected_dir = 45
+    track = Tracking(expected_x, expected_y, expected_dir)
+    assert track.x == expected_x
+    assert track.y == expected_y
+    assert track.direction == expected_dir
+
+def test_tracking_first_quad():
+    """turn 90 deg from the center origin(0,0)"""
+    point = Point(5, 0)
+    direction = 90
+    origin_y = 0
+    track = Tracking(0, origin_y, 0)
+    track.update_position(point, direction)
+    assert track.x == point.x
+    assert track.y == point.y
+    assert  track.direction == direction
+
+def test_tracking_south_full_reversal():
+    """facing down and turn around"""
+
+def test_course_first_quad():
+    """test from (0,0) to (1,1)"""
+    line = LineSegment(Point(0,0), Point(1,1))
+    track = Tracking(0,0,0)
+    track.course([line])
+    assert track.x == 1
+    assert track.y == 1
+    assert track.direction == 45
+
+# def test_course_first_quad_return_origin():
+#     line = LineSegment(Point(1,1), Point(0,0))
+#     track = Tracking(1,1,45)
+#     track.course([line])
+#     assert track.x == 0
+#     assert track.y ==0
+#     assert track.direction == 225
