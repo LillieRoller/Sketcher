20a4dcfbc4068549fb58aafd4fb86694b4e40cc7
Lillie Roller <lillie.rolle@gmail.com>
2023-07-01 14:13:16 +0200

    can draaw a quare from svg

--- src/sketcher.py
+++ src/sketcher.py
@@ -6,28 +6,40 @@ from irobot.codelevel3 import navigate_to, set_marker, Marker
 import irobot.ble_connection as ble_connection
 from irobot.robot_navigation import Point
 
-def draw_path(path:str):
-	"""will recieve a svg path and draw the path commands"""
-	route = path_to_route(path_d_attribute = path)
-	for segment in route.segments:
-		if segment.type == SegmentType.MOVE:
-			print(set_marker(Marker.UP))
-		elif segment.type == SegmentType.LINE:
-			print(set_marker(Marker.DOWN))
-		
-		print(navigate_to(x = segment.coordinate.x, y = segment.coordinate.y))
-
-
+# def convert_path_to_points(path:str):
+#     """Converts an SVG path to a list of points."""
+#     route = path_to_route(path_d_attribute=path)
+#     points = []
+#     for segment in route.segments:
+#         if segment.type == SegmentType.MOVE:
+#             points.append(Point(segment.coordinate.x, segment.coordinate.y))
+#         elif segment.type == SegmentType.LINE:
+#             points.append(Point(segment.coordinate.x, segment.coordinate.y))
+#     return points
+def convert_path_to_points(path:str):
+    """Converts an SVG path to a list of points."""
+    route = path_to_route(path_d_attribute=path)
+    points = []
+    for segment in route.segments:
+        if segment.type == SegmentType.MOVE:
+            points.append(Point(segment.coordinate.x, segment.coordinate.y))
+        elif segment.type == SegmentType.LINE:
+            points.append(Point(segment.coordinate.x, segment.coordinate.y))
+    return points
 
 def main():
 	"""Puts together all that is needed to complete a drawing."""
-	# draw_path(path = "M 0 0 H 32 V 32 H 0 L 0 0")
-	points=[
-		Point(16,16),
-		Point(-16,16),
-		Point(16,-16),
-		Point(0,0),
-	]
+	path = "M 0 0 H 16 V 16 H 0 L 0 0"  # Example SVG path
+	points = convert_path_to_points(path)
+	print(points)  # Or pass 'points' to the robot for further processing
+	# points=[
+	# Point(16,16),
+	# Point(16,-16),
+	#  (-16,-16),
+	# Point(-16,16),
+	# Point(16,16),
+	# Point(0,0),
+	# ]
 	ble_connection.run(points)
 
 if __name__ == "__main__":
